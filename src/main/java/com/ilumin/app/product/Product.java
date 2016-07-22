a772e1c92e876ca41c433e93521c407f79914c2e
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-22 09:03:10 +0700

    UPDATE product entity apply lombok

--- src/main/java/com/ilumin/app/product/Product.java
+++ src/main/java/com/ilumin/app/product/Product.java
@@ -1,18 +1,26 @@
 package com.ilumin.app.product;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Product {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Version
     private Integer version;
 
+    @Getter @Setter
     private String title;
+
+    @Getter @Setter
     private Double price = 0.0;
 
 }
