4f347c8cc0388eed8d158c9ac3b604ed0d4524f0
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-22 11:50:04 +0700

    UPDATE product entity - add getter setter

--- src/main/java/com/ilumin/app/product/Product.java
+++ src/main/java/com/ilumin/app/product/Product.java
@@ -1,27 +1,28 @@
 package com.ilumin.app.product;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 @Entity
 public class Product {
 
-    @Getter
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Version
-    private Integer version;
-
-    @Getter @Setter
+    @NotNull
     private String title;
 
-    @Getter @Setter
+    @NotNull
+    @Min(0)
     private BigDecimal price;
 
     protected Product() {
@@ -31,6 +32,22 @@ public class Product {
     public Product(String title, BigDecimal price) {
         this();
         this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 }
