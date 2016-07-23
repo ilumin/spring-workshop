36bcc345408874f75f5cd17e6348a1ea2d7cc30d
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-23 13:19:05 +0700

    ADD tag entity

---
+++ src/main/java/com/ilumin/app/tag/Tag.java
@@ -0,0 +1,48 @@
+package com.ilumin.app.tag;
+
+import com.ilumin.app.product.Product;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "product_tags",
+            joinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id", referencedColumnName = "id")
+    )
+    private Set<Product> products;
+
+    protected Tag() {
+        this.id = null;
+    }
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
