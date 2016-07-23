80634f358a9d1cb9b38549bda7ed2dd3eb152184
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-24 06:39:03 +0700

    UPDATE product info projection to displat tags

--- src/main/java/com/ilumin/app/product/projection/ProductInfo.java
+++ src/main/java/com/ilumin/app/product/projection/ProductInfo.java
@@ -2,11 +2,16 @@ package com.ilumin.app.product.projection;
 
 import com.ilumin.app.category.Category;
 import com.ilumin.app.product.Product;
+import com.ilumin.app.tag.Tag;
 import org.springframework.data.rest.core.config.Projection;
+
+import java.math.BigDecimal;
+import java.util.Set;
 
 @Projection(name = "info", types = { Product.class })
 public interface ProductInfo {
     String getTitle();
-    String getPrice();
+    BigDecimal getPrice();
     Category getCategory();
+    Set<Tag> getTags();
 }
